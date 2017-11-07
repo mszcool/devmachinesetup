8e23bff985dd43ae1a6e27d38159927e33130d77
Mario Szpuszta <marioszp@microsoft.com>
2017-11-07 15:39:22 +0100

    Fixing .NET Core 2.0 on Ubuntu 16.04 vs 17.04

--- Install-Ubuntu.sh
+++ Install-Ubuntu.sh
@@ -186,6 +186,10 @@ case "$instDotNetCore" in
     case "2.0")
         curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
         sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+        # Ubuntu 16.04
+        #sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+        #sudo apt-get update
+        # Ubuntu 17.04
         sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-zesty-prod zesty main" > /etc/apt/sources.list.d/dotnetdev.list'
         sudo apt-get update
         sudo apt-get install -y dotnet-sdk-2.0.0
