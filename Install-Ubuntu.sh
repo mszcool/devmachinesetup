3d0249e5a24a2b3ab82922953bdce9689df0dc75
Mario Szpuszta <marioszp@microsoft.com>
2020-06-26 20:01:42 +0200

    Continue Testing #7

--- Install-Ubuntu.sh
+++ Install-Ubuntu.sh
@@ -435,7 +435,6 @@ if [ $instPrompt == 1 ]; then
 
     go get -u github.com/justjanne/powerline-go
 
-asldkfjasldkkgh
     goPathExists=$(grep GOPATH ~/.bashrc)
     if [ ! "$goPathExists" ]; then
         # shellcheck disable=SC1090
