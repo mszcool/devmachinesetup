25b084f44d1b28887ed41b745a040b9fe4ba1b6d
Mario Szpuszta <mszcool@users.noreply.github.com>
2022-09-16 20:19:54 +0200

    Updating after re-install desktop
    Updating and optimizing the script to support both, fully-fledged Windows Dev or Windows Dev with primary dev tools on WSL 2

--- Install-WindowsMachine.ps1
+++ Install-WindowsMachine.ps1
@@ -10,8 +10,9 @@ Param
     [Switch]
     $userTools,
 
-    [Switch]
-    $ittools,
+    [Parameter(Mandatory = $False)]
+    [ValidateSet("no", "all", "basic")]
+    $ittools = "no",
 
     [Switch]
     $dev,
@@ -29,22 +30,20 @@ Param
     [Switch]
     $hyperv,
 
-    [Switch]
-    $instVsCode,
-
-    [Switch]
-    $installVs,
+    [Parameter(Mandatory = $False)]
+    [ValidateSet("no", "plain", "full")]
+    $vsCode = "no",
 
     [Parameter(Mandatory = $False)]
-    [ValidateSet("Community", "Professional", "Enterprise")]
-    $vsEdition = "Community",
+    [ValidateSet("no", "Community", "Professional", "Enterprise")]
+    $visualStudio = "no",
 
     [Parameter(Mandatory = $False)]
-    [ValidateSet("none", "intelliJ", "eclipse-sts", "all")]
-    $installOtherIDE = "none",
+    [ValidateSet("no", "intelliJ", "eclipse-sts", "all")]
+    $otherIde = "no",
 
     [Switch]
-    $instPrettyPrompt
+    $prettyPrompt
 
 )
 
@@ -53,55 +52,6 @@ Param
 Write-Information "***********************************************************************************************************"
 Write-Information "Please make sure to have winget installed: https://docs.microsoft.com/en-us/windows/package-manager/winget/"
 Write-Information "***********************************************************************************************************"
-
-
-#
-# Store the location of the original script execution
-#
-$originalExecPath = Get-Location
-
-
-#
-# Simple Parameter validation
-#
-if ( $prepOS -and ($tools -or $userTools -or $ittools -or $dev -or $devTools -or ( $docker -ne "no" ) -or $clis -or $instVsCode -or $installVs -or ( $installOtherIDE -ne "none" ) -or $instPrettyPrompt) ) {
-    throw "Running the script with -prepOS does not allow you to use any other switches. First run -prepOS and then run with any other allowed combination of switches!"
-}
-
-
-#
-# [prepOS] Installing Operating System Components as well as chocolatey itself. Needs to happen before ANY other runs!
-#
-if ( $prepOS ) {
-    # Enable Console Prompting for PowerShell
-    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name "ConsolePrompting" -Value $True
-
-    # Install git using winget
-    winget install --source winget --silent "Git.Git"
-    winget install --source winget --silent "Git Large File Storage"
-
-    # Install Scoop, which is more convenient for CLIs and command line dev tools
-    Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh')
-
-    # Install Windows Features
-    Enable-WindowsOptionalFeature -FeatureName NetFx4-AdvSrvs -Online -NoRestart
-    Enable-WindowsOptionalFeature -FeatureName NetFx4Extended-ASPNET45 -Online -NoRestart
-    Enable-WindowsOptionalFeature -FeatureName WCF-Services45 -Online -NoRestart
-    Enable-WindowsOptionalFeature -FeatureName WCF-TCP-PortSharing45 -Online -NoRestart
-    Enable-WindowsOptionalFeature -FeatureName HypervisorPlatform -Online -NoRestart
-    Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -NoRestart
-
-    if ( $hyperv ) {
-        Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online -NoRestart
-        Enable-WindowsOptionalFeature -FeatureName Containers -Online -NoRestart
-    }
-
-    Write-Information ""
-    Write-Information "Installation of OS components completed, please restart your computer once ready!"
-    Write-Information ""
-
-    Exit
-}
 
 
 #
@@ -140,6 +90,58 @@ function PreparePowerShell() {
     #Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     Write-Information "Installing PowerShellGet if neeed..."
     Install-Module -Name PowerShellGet -Force -AllowClobber
+}
+
+
+#
+# Store the location of the original script execution
+#
+$originalExecPath = Get-Location
+
+
+#
+# Simple Parameter validation
+#
+if ( $prepOS -and ($tools -or $userTools -or ( $ittools -ne "no" ) -or $dev -or $devTools -or ( $docker -ne "no" ) -or $clis -or ( $vsCode -ne "no" )  -or ( $visualStudio -ne "no" ) -or ( $otherIde -ne "no" ) -or $prettyPrompt) ) {
+    throw "Running the script with -prepOS does not allow you to use any other switches. First run -prepOS and then run with any other allowed combination of switches!"
+}
+
+
+#
+# [prepOS] Installing Operating System Components as well as chocolatey itself. Needs to happen before ANY other runs!
+#
+if ( $prepOS ) {
+    # Enable Console Prompting for PowerShell
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds" -Name "ConsolePrompting" -Value $True
+
+    # Install git using winget
+    winget install --source winget --silent "Git.Git"
+    winget install --source winget --silent "Git Large File Storage"
+
+    RefreshEnvPath
+
+    # Install Scoop, which is more convenient for CLIs and command line dev tools
+    Invoke-RestMethod "https://get.scoop.sh" -outfile "$env:TEMP\installScoop.ps1"
+    Invoke-Expression -Command "$env:TEMP\installScoop.ps1 -RunAsAdmin"
+
+    # Install Windows Features
+    Enable-WindowsOptionalFeature -FeatureName NetFx4-AdvSrvs -Online -NoRestart
+    Enable-WindowsOptionalFeature -FeatureName NetFx4Extended-ASPNET45 -Online -NoRestart
+    Enable-WindowsOptionalFeature -FeatureName WCF-Services45 -Online -NoRestart
+    Enable-WindowsOptionalFeature -FeatureName WCF-TCP-PortSharing45 -Online -NoRestart
+    Enable-WindowsOptionalFeature -FeatureName HypervisorPlatform -Online -NoRestart
+    Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -NoRestart
+
+    if ( $hyperv ) {
+        Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online -NoRestart
+        Enable-WindowsOptionalFeature -FeatureName Containers -Online -NoRestart
+    }
+
+    Write-Information ""
+    Write-Information "Installation of OS components completed, please restart your computer once ready!"
+    Write-Information ""
+
+    Exit
 }
 
 
@@ -201,8 +203,6 @@ if ( $userTools ) {
 
     winget install --source msstore --silent --accept-package-agreements "QuickLook"
     
-    winget install --source msstore --silent --accept-package-agreements "EarTrumpet"
-
     winget install --source msstore --silent --accept-package-agreements "WorkingHours" # "WorkingHours — Time Tracking / Timesheet"
 
     winget install --source msstore --silent --accept-package-agreements "Netflix"
@@ -223,9 +223,9 @@ if ( $userTools ) {
 
     winget install --source msstore --silent --accept-package-agreements "MSN Weather"
 
-    winget install --source msstore --silent --accept-package-agreements "Surface Audio"
+    #winget install --source msstore --silent --accept-package-agreements "Surface Audio"
 
-    winget install --source msstore --silent --accept-package-agreements "Dynamics AX 2012 Expenses"
+    #winget install --source msstore --silent --accept-package-agreements "Dynamics AX 2012 Expenses"
 
     winget install --source msstore --silent --accept-package-agreements "Speedtest by Ookla"
 
@@ -239,7 +239,7 @@ if ( $userTools ) {
 #
 # [ittools] IT-oriented tools
 #
-if ( $ittools ) {
+if ( ($ittools -eq "all") -or ($ittools -eq "basic") ) {
 
     winget install --source winget --silent "gerardog.gsudo"
     
@@ -256,6 +256,10 @@ if ( $ittools ) {
     winget install --source msstore --silent --accept-package-agreements "Ubuntu 18.04 LTS"
 
     winget install --source msstore --silent --accept-package-agreements "Ubuntu 20.04 LTS"
+
+}
+
+if ( $ittools -eq "all" ) {
 
     # Scoop based packages
 
@@ -446,7 +450,7 @@ if ( $clis ) {
 #
 # Visual Studio Code Extensions
 #
-if ( $instVsCode ) {
+if ( ($vsCode -eq "plain") -or ($vsCode -eq "full") ) {
 
     # First install Visual Studio Code
     winget install --source winget --silent "Microsoft.VisualStudioCode"
@@ -454,18 +458,19 @@ if ( $instVsCode ) {
     # Refresh the Path
     RefreshEnvPath
 
-    # Start installing all extensions
-    $vsCodeExtensions = Get-Content "$originalExecPath\vscode.extensions"
-    $vsCodeExtensions | ForEach-Object { code --install-extension $_ }
-
+    if ( $vsCode -eq "full" ) {
+        # Start installing all extensions
+        $vsCodeExtensions = Get-Content "$originalExecPath\vscode.extensions"
+        $vsCodeExtensions | ForEach-Object { code --install-extension $_ }
+    }
 }
 
 
 #
 # [installVs] Installing a version of Visual Studio (based on Chocolatey)
 #
-if ($installVs) {
-    winget install --source winget --silent "Visual Studio $vsEdition"
+if ($visualStudio -ne "no") {
+    winget install --source winget --silent "Visual Studio $visualStudio"
 }
 
 
@@ -490,14 +495,14 @@ if (($installOtherIDE -eq "all") -or ($installOtherIDE -eq "eclipse-sts")) {
 #
 # Installing a pretty prompt for PowerShell
 #
-if ( $instPrettyPrompt ) {
+if ( $prettyPrompt ) {
 
     PreparePowerShell
 
     Write-Information "Installing posh-git...."
     Install-Module -Name posh-git -Force
     Write-Information "Installing oh-my-posh..."
-    Install-Module -Name oh-my-posh -Force
+    winget install --silent --source winget --id JanDeDobbeleer.OhMyPosh
     Write-Information "Installing PSReadLine..."
     Install-Module -Name PSReadLine -SkipPublisherCheck -Force
 
@@ -510,20 +515,11 @@ if ( $instPrettyPrompt ) {
     # A bit hacky, but this is a bit of spare-time, hence limited time to optimize:)
     $profileContent = Get-Content -Path $PROFILE
     if ( [System.String]::IsNullOrEmpty($profileContent) ) { $profileContent = "" }
-    if ( ! $profileContent.Contains("posh-git") ) { Add-Content -Path $PROFILE -Value "Import-Module posh-git" }
-    if ( ! $profileContent.Contains("oh-my-posh") ) { Add-Content -Path $PROFILE -Value "Import-Module oh-my-posh" }
-    if ( ! $profileContent.Contains("Set-Theme Paradox") ) { Add-Content -Path $PROFILE -Value "Set-Theme Paradox" }
+    if ( ! $profileContent.Contains("posh-git") ) { Add-Content -Path $PROFILE -Value 'Import-Module posh-git' }
+    if ( ! $profileContent.Contains("shellName") ) { Add-Content -Path $PROFILE -Value '$shellName = $(oh-my-posh get shell)' }
+    if ( ! $profileContent.Contains("oh-my-posh") ) { Add-Content -Path $PROFILE -Value 'oh-my-posh init $shellName --config "$env:POSH_THEMES_PATH\iterm2.omp.json" | Invoke-Expression' }
 
-    # Download the Cascadia Font PL true type font
-    $fontUrl = "https://github.com/microsoft/cascadia-code/releases/download/v2005.15/CascadiaCode_2005.15.zip"
-    $fontZipFile = "$env:TEMP\CascadiaCode.zip"
-    $fontOutDir = "$env:TEMP\CascadiaCode"
-    Invoke-WebRequest -Uri $fontUrl -OutFile $fontZipFile
-    ExtractZipArchive -zipFile $fontZipFile -outPath $fontOutDir
-
-    # Install the Cascadia Code PL Font
-    $fontFile = [System.IO.Path]::Combine($fontOutDir, "ttf\CascadiaCodePL.ttf")
-    Copy-Item "$fontFile" "$env:windir\Fonts"
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" -Name "Cascadia Code PL" -PropertyType String -Value "CascadiaCodePL.ttf"
-    Write-Information "You need to restart to make the font-installation effective (used this way instead of shell object as it works on server core)!"
+    # Install the fonts with oh-my-posh
+    oh-my-posh font install Meslo
+    oh-my-posh font install CascadiaCode
 }
